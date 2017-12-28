26edb079908cc097550c560143dbd64c1f53a106
wostraq <wostraqwebsite@gmail.com>
2017-12-28 10:58:52 +0000

    Create test_website.py

---
+++ website/test_website.py
@@ -0,0 +1,23 @@
+from . import content 
+
+
+def test_home():
+  content.home()
+  
+@blueprint.route('/about/membership')
+def test_about_membership():
+  content.about_membership()
+
+def test_about_local():
+  content.about_local()
+
+def test_about_committee():
+  content.about_committe()
+  
+def test_projects_previous():
+  content.projects_previous()
+
+
+def test_projects_propose():
+  content.projects_propose()
+
